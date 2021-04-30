a626bb193559b9a397de89dd06909d7685a953e1
svna1 <wild_rose@ua.fm>
2021-04-30 12:58:31 +0300

    add customer-service

--- credit-score-controller/src/main/java/com/demo/creditscore/controller/exception/GlobalExceptionAdvice.java
+++ credit-score-controller/src/main/java/com/demo/creditscore/controller/exception/GlobalExceptionAdvice.java
@@ -1,10 +1,26 @@
 package com.demo.creditscore.controller.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.ConversionFailedException;
+import org.springframework.data.mapping.PropertyReferenceException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Slf4j
 @ControllerAdvice(basePackages = "com.demo.creditscore.controller")
 public class GlobalExceptionAdvice {
-
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({MethodArgumentTypeMismatchException.class, MethodArgumentNotValidException.class, ConversionFailedException.class,
+            PropertyReferenceException.class, IllegalArgumentException.class})
+    public ResponseEntity<String> catchIllegalArgumentException(Exception e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ExceptionMessage.REQUEST_VALIDATION_FAILED.getMessage());
+    }
 }
